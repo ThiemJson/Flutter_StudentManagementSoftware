7f2eb43c842e6bee0b0357cd686965c96b921cfd
JasonT <thiemjason@flutter.com>
2020-08-08 14:45:35 +0700

    What is deifference between virtual devices and real devives,, wtf ???

--- lib/Responsive.dart
+++ lib/Responsive.dart
@@ -11,4 +11,54 @@ import 'package:flutter/material.dart';
 //  }
 //}
 
+class MyApp_Res extends StatefulWidget {
+  @override
+  _MyApp_ResState createState() => _MyApp_ResState();
+}
 
+class _MyApp_ResState extends State<MyApp_Res> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Search Student",
+      theme: ThemeData(
+          //primaryColor: Color(0xFF832685),
+          //primaryColorLight: Color(0xFFC81379),
+          //accentColor: Color(0xFFFAF2F8)
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Text Text_Appbar = new Text("Test Gradiant", style: TextStyle(fontSize: 25, color: Colors.white),);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 60,
+        title: Text("Test Grad", style: TextStyle(fontSize: 25, color: Colors.white),),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              /*gradient: LinearGradient(colors: [Colors.red, Colors.blue],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  )*/
+              gradient: LinearGradient(begin: Alignment.topCenter, colors: [Color(0xFF832685),Color(0xFFC81379)])
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text("Hello Wordld"),
+      ),
+    );
+  }
+}
--- lib/SearchStudent.dart
+++ lib/SearchStudent.dart
@@ -2,6 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:student_management_software/StudentInfor.dart';
 import 'Responsive.dart';
+
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Search Student",
+      theme: ThemeData(
+        primaryColor: Color(0xFF832685),
+        primaryColorLight: Color(0xFFC81379),
+        accentColor: Color(0xfffaf2f8)
+      ),
+      home: SearhStudent(),
+    );
+  }
+}
+
+
+
 class SearhStudent extends StatefulWidget {
   @override
   _SearhStudentState createState() => _SearhStudentState();
@@ -10,21 +36,25 @@ class SearhStudent extends StatefulWidget {
 class _SearhStudentState extends State<SearhStudent> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "SearchStudent",
-      home: Scaffold(
-        //backgroundColor: Colors.lime,
-        appBar: AppBar(
-          elevation: 50,
-          centerTitle: true,
-          backgroundColor: Colors.indigoAccent,
-          title: Text("Search Student", style: TextStyle(fontSize: 30, color: Colors.white),),
-          leading: Icon(
-            Icons.account_circle, color: Colors.white, size: 40,
+    return Scaffold(
+      //backgroundColor: Colors.lime,
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: false,
+        //backgroundColor: Color(0xFF832685),
+//        backgroundColor: Colors.indigoAccent,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [Color(0xFF832685),Color(0xFFC81379)])
           ),
         ),
-        body: SafeArea(
-          //minimum: EdgeInsets.all(10),
+        title: Text("Search Student", style: TextStyle(fontSize: 30, color: Colors.white),),
+        leading: Icon(
+          Icons.account_circle, color: Colors.white, size: 40,
+        ),
+      ),
+      body: SafeArea(
+        //minimum: EdgeInsets.all(10),
           child: Container(
             width: 1000,
             height: 1000,
@@ -53,25 +83,25 @@ class _SearhStudentState extends State<SearhStudent> {
                     children: <Widget>[
                       StudentInfor('Nguyễn Cao Thiêm', "60TH3", "1851061743", "Information Technology", "Thuyloi University",true),
                       StudentInfor('Nguyễn Văn A', "60HT", "1851161111", "Công tác quốc tế", "Đại học Sư phạm Hà Nội",false),
-                      StudentInfor('Nguyễn Ngọc Minh Châu', "60TH1", "1851160151", "Information Technology", "Thuyloi University",true),
+                      StudentInfor('Nguyễn Văn Dũng', "60TH1", "1851160151", "Information Technology", "Thuyloi University",true),
                       StudentInfor('Nguyễn Cao Thiêm', "60TH3", "1851061743", "Information Technology", "Thuyloi University",false),
                     ],
                   ),
-                )
+                ),
+
               ],
             ),
           )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.search, color: Colors.white, size: 21,),
+        onPressed: null,
+        elevation: 29,
+        backgroundColor: Colors.indigo,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
         ),
-        floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.search, color: Colors.white, size: 21,),
-          onPressed: null,
-          elevation: 29,
-          backgroundColor: Colors.indigo,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-          label: Text("SEARCH", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white,),),
-        ),
+        label: Text("SEARCH", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white,),),
       ),
     );
   }
